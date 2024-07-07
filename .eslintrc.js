01d044a808830c6e923a008be1b90a44d0c3230f
EARPHONE <15107544673@163.com>
2024-07-07 15:37:21 +0800

    feat: 基础配置修改

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,3 @@
 module.exports = {
-  env: {
-    node: true,
-    browser: true,
-    es2021: true
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'standard',
-    'standard-jsx'
-  ],
-  overrides: [
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  plugins: [
-    'react',
-    '@typescript-eslint'
-  ],
-  rules: {
-    // 自定义规则
-    'no-unused-vars': 'off',
-    'no-useless-constructor': 'off'
-  }
-}
+  extends: ['airbnb', 'airbnb/hooks'],
+};
